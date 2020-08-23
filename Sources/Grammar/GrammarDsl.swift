7790ad0bb317ea396e066a3ba4575ba483b1ebc9
Mikoláš Stuchlík <mikolas.stuchlik@futured.app>
2020-08-23 11:26:58 +0200

    Attempt to parse gir

--- Sources/Grammar/GrammarDsl.swift
+++ Sources/Grammar/GrammarDsl.swift
@@ -4,8 +4,8 @@ func →(_ nonTerminal: NonTerminal, _ prouct: Product) -> Production {
     Production(nonTerminal: nonTerminal, product: prouct)
 }
 
-func →(_ nonTerminal: NonTerminal, _ prouct: Product) -> Grammar {
-    Grammar(grammar: [Production(nonTerminal: nonTerminal, product: prouct)])
+func →(_ nonTerminal: NonTerminal, _ prouct: Product) -> GrammarDsl {
+    GrammarDsl(grammar: [Production(nonTerminal: nonTerminal, product: prouct)])
 }
 
 prefix operator %
@@ -20,12 +20,13 @@ prefix func ^(_ content: String) -> Terminal {
     Terminal(content: content)
 }
 
-infix operator .. : AdditionPrecedence
+infix operator .. : MultiplicationPrecedence
 
 func ..(_ lProduct: Product, _ rProduct: Product ) -> StringProduct {
     StringProduct(string: [lProduct, rProduct])
 }
 
+infix operator | : AdditionPrecedence
 func |(_ lProduct: Product, _ rProduct: Product) -> OptionProduct {
     OptionProduct(concat: lProduct, with: rProduct)
 }
@@ -65,10 +66,12 @@ struct NonTerminal: Product, Hashable {
 }
 
 struct Terminal: Product {
+    static let ε = Terminal(content: "")
+
     let content: String
 
     var description: String {
-        "'\(content)'"
+        "\"\(content.covfefeEscaped)\""
     }
 }
 
@@ -129,31 +132,57 @@ struct PositiveIterationProduct: Product {
     }
 }
 
-struct Grammar: CustomStringConvertible {
+struct GrammarDsl: CustomStringConvertible {
+    static let empty = GrammarDsl(grammar: [])
+    
     let grammar: [Production]
     
     var description: String {
         grammar.map { $0.description }.joined(separator: "\n")
     }
 
-    static func build(@GrammarBuilder builder: () -> Grammar) -> Grammar {
+    static func build(@GrammarBuilder builder: () -> GrammarDsl) -> GrammarDsl {
         builder()
+    }
+    
+    func union(with otherGrammar: GrammarDsl) -> GrammarDsl {
+        let components = self.grammar + otherGrammar.grammar
+        var newGrammar = [NonTerminal:Product]()
+        for item in components {
+            guard let existing = newGrammar[item.nonTerminal] else {
+                newGrammar[item.nonTerminal] = item.product
+                continue
+            }
+            
+            newGrammar[item.nonTerminal] = OptionProduct(concat: item.product, with: existing)
+        }
+        
+        return GrammarDsl(grammar: newGrammar.map(Production.init(nonTerminal:product:)))
+    }
+    
+    static func formUnion(from lGrammar: GrammarDsl, and rGrammar: GrammarDsl) -> GrammarDsl {
+        lGrammar.union(with: rGrammar)
+    }
+}
+
+extension String {
+    var covfefeEscaped: String {
+        self.map { char -> String in
+            switch char {
+            case #"\"#:
+                return #"\\"#
+            case "\"":
+                return "\\\""
+            default:
+                return String(char)
+            }
+        }.joined()
     }
 }
 
 @_functionBuilder
 enum GrammarBuilder {
-    static func buildBlock(_ components: Production...) -> Grammar {
-        var grammar = [NonTerminal:Product]()
-        for item in components {
-            guard let existing = grammar[item.nonTerminal] else {
-                grammar[item.nonTerminal] = item.product
-                continue
-            }
-            
-            grammar[item.nonTerminal] = OptionProduct(concat: item.product, with: existing)
-        }
-        
-        return Grammar(grammar: grammar.map(Production.init(nonTerminal:product:)))
+    static func buildBlock(_ components: Production...) -> GrammarDsl {
+        return components.reduce(GrammarDsl.empty) { $0.union(with: GrammarDsl(grammar: [$1])) }
     }
 }
--- Tests/GrammarTests/GrammarTests.swift
+++ Tests/GrammarTests/GrammarTests.swift
@@ -4,7 +4,7 @@ import Covfefe
 
 final class GrammarTests: XCTestCase {
     func testExample() {
-        let grammarDsl = Grammar.build {
+        let grammarDsl = GrammarDsl.build {
             %"person"   → %"name" .. %"space" .. (%"name" .. %"space")-? .. %"surname"
             %"name"     → ^"Mikolas"
                         | ^"Jan"
@@ -24,7 +24,180 @@ final class GrammarTests: XCTestCase {
         
     }
 
+    func testPrecedence() {
+        let grammarDsl = GrammarDsl.build {
+            %"person"   → %"name" .. %"space" | %"name" | %"name2" .. ^"aaa"
+        }
+        print(grammarDsl.description)
+        
+    }
+    
+    // https://www.oasis-open.org/committees/relax-ng/compact-20020607.html
+    
+    func testRelaxNGCompact() {
+        let grammarDsl1 = GrammarDsl.build {
+            %"topLevel"
+                → (%"decl")* .. ( %"pattern" | (%"grammarContent")*)
+
+            %"decl"
+                →  ^"namespace" .. %"identifierOrKeyword" .. ^"=" .. %"namespaceUri"
+                | ^"default" .. ^"namespace" .. (%"identifierOrKeyword")-? .. ^"=" .. %"namespaceUri"
+                | ^"datatypes" .. %"identifierOrKeyword" .. ^"=" .. %"literal"
+
+            %"pattern"
+                → ^"element" .. %"nameClass" .. ^"{" .. %"pattern" .. ^"}"
+                | ^"attribute" .. %"nameClass" .. ^"{" .. %"pattern" .. ^"}"
+                | %"pattern" .. (^"," .. ^"pattern")+
+                | %"pattern" ..  (^"&" .. %"pattern")+
+                | %"pattern" .. (^"|" .. %"pattern")+
+                | %"pattern" .. ^"?"
+                | %"pattern" .. ^"*"
+                | %"pattern" .. ^"+"
+                | ^"list" .. ^"{" .. %"pattern" .. ^"}"
+                | ^"mixed" .. ^"{" .. %"pattern" .. ^"}"
+                | %"identifier"
+                | ^"parent" .. %"identifier"
+                | ^"empty"
+                | ^"text"
+                | (%"datatypeName")-? .. %"datatypeValue"
+                | %"datatypeName" .. (^"{" .. (%"param")* .. ^"}")-? .. (%"exceptPattern")-?
+                | ^"notAllowed"
+                | ^"externalRef" .. %"uri" .. (%"inherit")-?
+                | ^"grammar" .. ^"{" .. (%"grammarContent")* .. ^"}"
+                | ^"(" .. %"pattern" .. ^")"
+        }
+
+        let grammarDsl2 = GrammarDsl.build {
+            %"param"
+                → %"identifierOrKeyword" .. ^"=" .. %"literal"
+
+            %"exceptPattern"
+                → ^"-" .. %"pattern"
+
+            %"grammarContent"
+                → ^"start"
+                | %"define"
+                | ^"div" .. ^"{" .. (%"grammarContent")* .. ^"}"
+                | ^"include" .. %"uri" .. (%"inherit")-? .. (^"{" .. (%"includeContent")* .. ^"}")-?
+
+            %"includeContent"
+                → %"define"
+                | %"start"
+                | ^"div" .. ^"{" .. (%"includeContent")* .. ^"}"
+        }
+
+        let grammarDsl3 = GrammarDsl.build {
+            %"start"
+                → ^"start" .. %"assignMethod" .. %"pattern"
+
+            %"define"
+                → %"identifier" .. %"assignMethod" .. %"pattern"
+
+            %"assignMethod"
+                → ^"="
+                | ^"|="
+                | ^"&="
+
+            %"nameClass"
+                → %"name"
+                | %"nsName" .. (%"exceptNameClass")-?
+                | %"anyName" .. (%"exceptNameClass")-?
+                | %"nameClass" .. ^"|" .. %"nameClass"
+                | ^"(" .. %"nameClass" .. ^")"
+
+        }
+            
+        let grammarDsl4 = GrammarDsl.build {
+            %"name"
+                → %"identifierOrKeyword"
+                | %"CName"
+
+            %"exceptNameClass"
+                → ^"-" .. %"nameClass"
+
+            %"datatypeName"
+                → %"CName"
+                | ^"string"
+                | ^"token"
+
+            %"datatypeValue"
+                → %"literal"
+
+            %"uri"
+                → %"literal"
+
+            %"namespaceUri"
+                → %"literal"
+                | ^"inherit"
+
+            %"inherit"
+                → ^"inherit" .. ^"=" .. %"identifierOrKeyword"
+
+            %"identifierOrKeyword"
+                → ^"identifier"
+                | ^"keyword"
+
+        }
+                
+        let grammarDsl5 = GrammarDsl.build {
+            %"identifier"
+                → %"NCName" // TODO Set operation: (NCName - keyword)
+                | %"quotedIdentifier"
+
+            %"quotedIdentifier"
+                → ^"\\" .. %"NCName"
+
+            %"CName"
+                → %"NCName" .. ^":" .. %"NCName"
+
+            %"nsName"
+                → %"NCName" .. ^":*"
+
+            %"anyName"
+                → ^"*"
+
+            %"literal"
+                → (%"literalSegment")+
+
+            %"literalSegment"
+                → ^"\"" .. (%"Char")* .. ^"\""  //TODO Set operation: '"' (Char - '"')* '"'
+                | ^"'" .. (%"Char")* .. ^"'"
+
+            %"keyword"
+                → ^"attribute"
+                | ^"default"
+                | ^"datatypes"
+                | ^"div"
+                | ^"element"
+                | ^"empty"
+                | ^"externalRef"
+                | ^"grammar"
+                | ^"include"
+                | ^"inherit"
+                | ^"list"
+                | ^"mixed"
+                | ^"namespace"
+                | ^"notAllowed"
+                | ^"parent"
+                | ^"start"
+                | ^"string"
+                | ^"text"
+                | ^"token"
+        }
+        
+        let finalGrammar = [grammarDsl1, grammarDsl2, grammarDsl3, grammarDsl4, grammarDsl5].reduce(GrammarDsl.empty, GrammarDsl.formUnion(from:and:))
+        
+        let grammar = try! Grammar(ebnf: finalGrammar.description, start: "topLevel")
+        
+        let parser = EarleyParser(grammar: grammar)
+
+        let syntaxTree = try! parser.syntaxTree(for: gir)
+        
+    }
+
     static var allTests = [
         ("testExample", testExample),
+        ("testRelaxNGCompact", testRelaxNGCompact),
+        ("testPrecedence", testPrecedence)
     ]
 }
